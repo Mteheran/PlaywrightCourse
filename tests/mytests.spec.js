4908ebcfb7ae5f8cd27b10ec3c0ee21606e2a172
Miguel Teheran <miguelteheran@gmail.com>
2024-02-21 16:36:47 -0500

    adding scrrenshot demo

--- tests/mytests.spec.js
+++ tests/mytests.spec.js
@@ -7,5 +7,9 @@ test("Login demo", async({page}) => {
     await page.getByRole("textbox", { name: "Password" }).fill("secret_sauce");
     await page.getByRole("button", {name: "Login"}).click();
 
+    await page.screenshot({path: 'saucedemoportal.png', fullPage: true});
+
+    await page.getByText("Swag Labs").screenshot({path: "titlescreenshot.png"});
+
     await expect(await page.getByText("Products")).toBeVisible();
 })

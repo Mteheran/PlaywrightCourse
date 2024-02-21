58dd066edee7486aed5d7d97a8286b76708cb82e
Miguel Teheran <miguelteheran@gmail.com>
2024-02-21 16:56:12 -0500

    using css and Id

--- tests/mytests.spec.js
+++ tests/mytests.spec.js
@@ -12,4 +12,14 @@ test("Login demo", async({page}) => {
     await page.getByText("Swag Labs").screenshot({path: "titlescreenshot.png"});
 
     await expect(await page.getByText("Products")).toBeVisible();
-})
+});
+
+test("Login demo by css class and Id", async ({ page }) => {
+  await page.goto("https://www.saucedemo.com/");
+  await page.locator("#user-name").fill("standard_user");
+  await page.locator("#password").fill("secret_sauce");
+  await page.locator("#login-button").click();
+  const productsTitle = await page.locator(".title");
+  await expect(productsTitle).toHaveText("Products");
+  await expect(productsTitle).toBeVisible();
+});
